e4d829622f55c151c5056dcf4d179bd058a614b7
gerardasb <gerardas.barstys@gmail.com>
2022-09-27 12:36:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const Invasion = '1 Jan 2024'
+
+function countdown() {
+  const InvasionDate = new Date(Invasion);
+  const currentDate = new Date();
+
+  const totalSeconds = (InvasionDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? (`0${time}`) : time;
+}
+
+countdown(days, hours, minutes, seconds);
+
+setInterval(countdown, 1000);
